968d6d1097565acc301064d239b4d39219dbd9aa
TangYang <1553604051@qq.com>
2026-06-11 17:25:06 +0800

    add：在仓库中封装了'退出登陆'逻辑副作用函数

--- src/stores/userinfo.ts
+++ src/stores/userinfo.ts
@@ -8,16 +8,23 @@ export const useUserInfoStore = defineStore(
   'userinfo',
   () => {
     const userInfo = ref<IUserInfo>({} as IUserInfo)
-    async function loginAction(account: IAccount) {
+    // 登录操作
+    const loginAction = async (account: IAccount) => {
       const res = await login(account)
       await useTokenStore().setToken(res.token)
       // 登录成功后，获取用户信息
       const userInfoRes = await getUserInfoById(res.id)
       userInfo.value = userInfoRes
     }
+    // 退出登录操作
+    const loginoutAction = async () => {
+      await useTokenStore().removeToken()
+      userInfo.value = {} as IUserInfo
+    }
     return {
       userInfo,
       loginAction,
+      loginoutAction,
     }
   },
   {
