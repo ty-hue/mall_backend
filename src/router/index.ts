cf4ace7c36958aa63ff64899cdfe99095bec9d0d
TangYang <1553604051@qq.com>
2026-06-11 17:19:28 +0800

    add：实现了全局路由导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
+import { useTokenStore } from '@/stores/token'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { storeToRefs } from 'pinia'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,6 +25,17 @@ const router = createRouter({
       component: () => import('@/views/not-found/NotFound.vue'),
     },
   ],
+})
+const whiteList = ['Login', 'NotFound']
+router.beforeEach((to, from, next) => {
+  console.log(to)
+
+  const { token } = storeToRefs(useTokenStore())
+  if (!whiteList.includes(to.name as string) && !token.value) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
 })
 
 export default router
