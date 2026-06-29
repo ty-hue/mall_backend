3cf92ffcbe537a4a1c4af23be3fd738af0b5d644
TangYang <1553604051@qq.com>
2026-06-30 00:11:42 +0800

    fix：将system目录下的根组件的公共逻辑抽离成hook进行逻辑复用（代码优化）

---
+++ src/hooks/useSystemRootPageHook.ts
@@ -0,0 +1,39 @@
+import { getPageList } from '@/service/apis/main'
+import { ref } from 'vue'
+
+export const useSystemRootPageHook = () => {
+  /** 泛型组件无法用 InstanceType，手动声明暴露接口 */
+  interface SearchExposed {
+    formData: Record<string, unknown>
+  }
+  interface ContentExposed {
+    pagination: { currentPage: number; pageSize: number }
+    loadData: (isInit?: boolean, isSearch?: boolean) => void
+  }
+
+  const searchRef = ref<SearchExposed | null>(null)
+  const contentRef = ref<ContentExposed | null>(null)
+
+  const loadPageData = async <T extends Record<string, unknown>>(
+    url: string,
+    preCallback?: () => void,
+    nextCallback?: (res: { list: T[]; total: number }) => void,
+    isInit?: boolean,
+    isAll?: boolean,
+  ) => {
+    preCallback?.()
+    const res = await getPageList<T>(url, {
+      ...(isAll ? {} : contentRef.value?.pagination),
+      condition: isInit ? {} : (searchRef.value?.formData as Partial<T>),
+    })
+    nextCallback?.(res)
+  }
+
+  const search = () => contentRef.value?.loadData(false, true)
+  return {
+    search,
+    loadPageData,
+    searchRef,
+    contentRef,
+  }
+}
--- src/views/main/system/department/department.vue
+++ src/views/main/system/department/department.vue
@@ -17,40 +17,13 @@
 <script setup lang="ts">
 import PageSearch from '@/components/page-search.vue'
 import PageContent from '@/components/page-content.vue'
-import { getPageList } from '@/service/apis/main.ts'
-import { ref } from 'vue'
 import { searchConfig } from './config/search.config.ts'
 import { contentConfig } from './config/content.config.ts'
 import { modalConfig } from './config/modal.config.ts'
 
-/** 泛型组件无法用 InstanceType，手动声明暴露接口 */
-interface SearchExposed {
-  formData: Record<string, unknown>
-}
-interface ContentExposed {
-  pagination: { currentPage: number; pageSize: number }
-  loadData: (isInit?: boolean, isSearch?: boolean) => void
-}
+import { useSystemRootPageHook } from '@/hooks/useSystemRootPageHook.ts'
 
-const searchRef = ref<SearchExposed | null>(null)
-const contentRef = ref<ContentExposed | null>(null)
-
-const loadPageData = async <T extends Record<string, unknown>>(
-  url: string,
-  preCallback?: () => void,
-  nextCallback?: (res: { list: T[]; total: number }) => void,
-  isInit?: boolean,
-  isAll?: boolean,
-) => {
-  preCallback?.()
-  const res = await getPageList<T>(url, {
-    ...(isAll ? {} : contentRef.value?.pagination),
-    condition: isInit ? {} : (searchRef.value?.formData as Partial<T>),
-  })
-  nextCallback?.(res)
-}
-
-const search = () => contentRef.value?.loadData(false, true)
+const { search, loadPageData, searchRef, contentRef } = useSystemRootPageHook()
 </script>
 
 <style lang="less" scoped>
